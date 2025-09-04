7aa4ac2b2482b16a46d390cb0290213a11427832
Amonimus <samparo96@gmail.com>
2025-09-04 19:01:06 +0300

    commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from FractalKeyboard import FractalKeyboard
 
 def main():
 	key_list: list = [0, 1, 0, 1, 0, 0, 1, 0, -1]   # Colors of a row
-	FractalKeyboard(key_list, 0, 800)
+	FractalKeyboard(key_list, 2, 800)
 
 
 if __name__ == '__main__':
